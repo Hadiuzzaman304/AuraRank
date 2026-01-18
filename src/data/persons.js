0202581227d26d787dd1a966d9a667a4a16406d7
Hadiuzzaman Ome <omee1233@gmail.com>
2026-01-19 02:46:32 +0600

    Fix image paths for Vercel (case-sensitive)

--- src/data/persons.js
+++ src/data/persons.js
@@ -1,24 +1,132 @@
 export const persons = [
-  "Bate",
-  "BenBer",
-  "Chrorh",
-  "Crevans",
-  "Elusk",
-  "Roder",
-  "SaltMan",
-  "Stobs",
-  "Thohild",
-  "Tolland",
-].map((name) => ({
-  id: name.toLowerCase(),
-  name,
-  images: {
-    front: [
-      `/images/${name}/front/f1.jpg`,
-      `/images/${name}/front/f2.jpg`,
-    ],
-    left: [`/images/${name}/left/l.jpg`],
-    right: [`/images/${name}/right/r.jpg`],
-    back: [`/images/${name}/back/b.jpg`],
+  {
+    id: "bate",
+    name: "Bate",
+    images: {
+      front: [
+        "/images/Bate/Front/f1.jpg",
+        "/images/Bate/Front/f2.jpg",
+      ],
+      left: ["/images/Bate/Left/l.jpg"],
+      right: ["/images/Bate/Right/r.jpg"],
+      back: ["/images/Bate/Back/b.jpg"],
+    },
   },
-}));
+  {
+    id: "benber",
+    name: "BenBer",
+    images: {
+      front: [
+        "/images/BenBer/Front/f1.jpg",
+        "/images/BenBer/Front/f2.jpg",
+      ],
+      left: ["/images/BenBer/Left/l.jpg"],
+      right: ["/images/BenBer/Right/r.jpg"],
+      back: ["/images/BenBer/Back/b.jpg"],
+    },
+  },
+  {
+    id: "chrorh",
+    name: "Chrorh",
+    images: {
+      front: [
+        "/images/Chrorh/Front/f1.jpg",
+        "/images/Chrorh/Front/f2.jpg",
+      ],
+      left: ["/images/Chrorh/Left/l.jpg"],
+      right: ["/images/Chrorh/Right/r.jpg"],
+      back: ["/images/Chrorh/Back/b.jpg"],
+    },
+  },
+  {
+    id: "crevans",
+    name: "Crevans",
+    images: {
+      front: [
+        "/images/Crevans/Front/f1.jpg",
+        "/images/Crevans/Front/f2.jpg",
+      ],
+      left: ["/images/Crevans/Left/l.jpg"],
+      right: ["/images/Crevans/Right/r.jpg"],
+      back: ["/images/Crevans/Back/b.jpg"],
+    },
+  },
+  {
+    id: "elusk",
+    name: "Elusk",
+    images: {
+      front: [
+        "/images/Elusk/Front/f1.jpg",
+        "/images/Elusk/Front/f2.jpg",
+      ],
+      left: ["/images/Elusk/Left/l.jpg"],
+      right: ["/images/Elusk/Right/r.jpg"],
+      back: ["/images/Elusk/Back/b.jpg"],
+    },
+  },
+  {
+    id: "roder",
+    name: "Roder",
+    images: {
+      front: [
+        "/images/Roder/Front/f1.jpg",
+        "/images/Roder/Front/f2.jpg",
+      ],
+      left: ["/images/Roder/Left/l.jpg"],
+      right: ["/images/Roder/Right/r.jpg"],
+      back: ["/images/Roder/Back/b.jpg"],
+    },
+  },
+  {
+    id: "saltman",
+    name: "SaltMan",
+    images: {
+      front: [
+        "/images/SaltMan/Front/f1.jpg",
+        "/images/SaltMan/Front/f2.jpg",
+      ],
+      left: ["/images/SaltMan/Left/l.jpg"],
+      right: ["/images/SaltMan/Right/r.jpg"],
+      back: ["/images/SaltMan/Back/b.jpg"],
+    },
+  },
+  {
+    id: "stobs",
+    name: "Stobs",
+    images: {
+      front: [
+        "/images/Stobs/Front/f1.jpg",
+        "/images/Stobs/Front/f2.jpg",
+      ],
+      left: ["/images/Stobs/Left/l.jpg"],
+      right: ["/images/Stobs/Right/r.jpg"],
+      back: ["/images/Stobs/Back/b.jpg"],
+    },
+  },
+  {
+    id: "thohild",
+    name: "Thohild",
+    images: {
+      front: [
+        "/images/Thohild/Front/f1.jpg",
+        "/images/Thohild/Front/f2.jpg",
+      ],
+      left: ["/images/Thohild/Left/l.jpg"],
+      right: ["/images/Thohild/Right/r.jpg"],
+      back: ["/images/Thohild/Back/b.jpg"],
+    },
+  },
+  {
+    id: "tolland",
+    name: "Tolland",
+    images: {
+      front: [
+        "/images/Tolland/Front/f1.jpg",
+        "/images/Tolland/Front/f2.jpg",
+      ],
+      left: ["/images/Tolland/Left/l.jpg"],
+      right: ["/images/Tolland/Right/r.jpg"],
+      back: ["/images/Tolland/Back/b.jpg"],
+    },
+  },
+];
